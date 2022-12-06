dc9fafd94060ee592cd8e856c2e6cd3ed8252cca
joe953 <joemazloum953@gmail.com>
2022-12-06 13:31:58 +0200

    city search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+    require 'uri'
+    require 'net/http'
+    require 'json'
+
+    def index
+        if(params[:q])
+            url = URI("http://dataservice.accuweather.com/locations/v1/cities/search?apikey=" + params[:apikey] + "&q=" + params[:q] + "&commit=Search")       
+            http = Net::HTTP.new(url.host, url.port)
+            request = Net::HTTP::Get.new(url)
+
+            response = http.request(request).body
+            @data = JSON.parse(response)
+        
+        else
+            @data = []
+        end
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  get "/:city", to: "application#selected_city", as: "selected_city"
+
   root "application#index"
 end
