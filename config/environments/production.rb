51c65c2039aafd2c0c2a5821918e27cc26dab23f
joe953 <joemazloum953@gmail.com>
2022-12-15 19:58:01 +0200

    ss

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,8 +7,8 @@ Rails.application.configure do
     address:  "smtp.gmail.com",
     port: 587,
     domain: "https://weather-ruby-on-rails.onrender.com",
-    username: "joemazloum953@gmail.com",
-    password: "ijejdmslcbkwptfa",
+    username: Rails.application.credentials.config.dig(:email),
+    password: Rails.application.credentials.config.dig(:password),
     authentication: "plain",
     enable_starttls_auto: true,
   }
